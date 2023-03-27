4a3c51b8b71c2221bce007501d15e028216b86bb
Alexis Thierry <alexist103@gmail.com>
2023-03-27 20:15:06 +0200

    oui

---
+++ 3DCard/app.js
@@ -0,0 +1,19 @@
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+
+
+container.addEventListener('mousemove', (e) => {
+	let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+	let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+	card.style.transform = `rotateX(${-yAxis}deg) rotateY(${xAxis}deg)`;
+}) 
+
+container.addEventListener('mouseenter', (e) => {
+	card.style.transition = 'none';
+})
+
+container.addEventListener('mouseleave', (e) => {
+	card.style.transition = 'all 0.5s ease';
+	card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+})
